061f371ca2281a56ab43c417cc6bfea8152aa4ca
Gabriel <leuping@gmail.com>
2019-06-10 15:47:53 -0700

    adding mobile problems

---
+++ mobile.py
@@ -0,0 +1,106 @@
+def mobile(left, right):
+    """Construct a mobile from a left side and a right side."""
+    return tree(None, [left, right])
+
+def sides(m):
+    """Select the sides of a mobile."""
+    return branches(m)
+
+def side(length, mobile_or_weight):
+    """Construct a side: a length of rod with a mobile or weight at the end."""
+    return tree(length, [mobile_or_weight])
+
+def length(s):
+    """Select the length of a side."""
+    return root(s)
+
+def end(s):
+    """Select the mobile or weight hanging at the end of a side."""
+    return branches(s)[0]
+
+def weight(size):
+    """Construct a weight of some size."""
+    assert size > 0
+    return tree(size, [])
+
+def size(w):
+    """Select the size of a weight."""
+    return root(w)
+
+def is_weight(w):
+    """Whether w is a weight, not a mobile."""
+    return not branches(w)
+
+def examples():
+    t = mobile(side(1, weight(2)),
+               side(2, weight(1)))
+    u = mobile(side(5, weight(1)),
+               side(1, mobile(side(2, weight(3)),
+                              side(3, weight(2)))))
+    v = mobile(side(4, t), side(2, u))
+    return (t, u, v)
+
+
+def total_weight(m):
+    """Return the total weight of m, a weight or mobile.
+    >>> t, u, v = examples()
+    >>> total_weight(t)
+    3
+    >>> total_weight(u)
+    6
+    >>> total_weight(v)
+    9
+    """
+    if is_weight(m):
+        return size(m)
+    else:
+        return sum([total_weight(end(s)) for s in sides(m)])
+
+def balanced(m):
+    """Return whether m is balanced.
+    >>> t, u, v = examples()
+    >>> balanced(t)
+    True
+    >>> balanced(v)
+    True
+    >>> w = mobile(side(3, t), side(2, u))
+    >>> balanced(w)
+    False
+    >>> balanced(mobile(side(1, v), side(1, w)))
+    False
+    >>> balanced(mobile(side(1, w), side(1, v)))
+    False
+    """
+    # hardest problem in this course, yet
+    # I think I'm just rusty cause I took a 1 week break
+
+    if is_weight(m):
+        return True
+    else:
+        b = [balanced(end(s)) for s in sides(m)]
+        weights = [total_weight(end(s)) for s in sides(m)]
+        lengths = [length(s) for s in sides(m)]
+
+        return all(b) and len(set(a * b for a, b in zip(lengths, weights))) == 1
+
+
+def with_totals(m):
+    """Return a mobile with total weights stored as the root of each mobile.
+    >>> t, _, v = examples()
+    >>> root(with_totals(t))
+    3
+    >>> print(root(t))                           # t should not change
+    None
+    >>> root(with_totals(v))
+    9
+    >>> [root(end(s)) for s in sides(with_totals(v))]
+    [3, 6]
+    >>> [root(end(s)) for s in sides(v)]         # v should not change
+    [None, None]
+    """
+    # assignment asked me to break the abstraction barrier here
+    if is_weight(m):
+        return tree(total_weight(m))
+    else:
+        stems = [tree(length(s), [with_totals(end(s))]) for s in sides(m)]
+        return tree(total_weight(m), stems)
