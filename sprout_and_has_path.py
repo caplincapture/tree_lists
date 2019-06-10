8ab170b9c468d5e8bf5ee071db8082f1ebb6dc32
Gabriel <leuping@gmail.com>
2019-06-10 16:26:58 -0700

    sprout_leaves_and_has_path

---
+++ sprout_and_has_path.py
@@ -0,0 +1,67 @@
+def sprout_leaves(t, vals):
+    """Sprout new leaves containing the data in vals at each leaf in
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    
+    
+def has_path(t, word):
+    """Return whether there is a path in a tree where the entries along the path
+    spell out a particular word.
+
+    >>> greetings = tree('h', [tree('i'),
+    ...                        tree('e', [tree('l', [tree('l', [tree('o')])]),
+    ...                                   tree('y')])])
+    >>> print_tree(greetings)
+    h
+      i
+      e
+        l
+          l
+            o
+        y
+    >>> has_path(greetings, 'h')
+    True
+    >>> has_path(greetings, 'i')
+    False
+    >>> has_path(greetings, 'hi')
+    True
+    >>> has_path(greetings, 'hello')
+    True
+    >>> has_path(greetings, 'hey')
+    True
+    >>> has_path(greetings, 'bye')
+    False
+
+    """
+    assert len(word) > 0, 'no path for empty words.'
+    "*** YOUR CODE HERE ***"
+    
